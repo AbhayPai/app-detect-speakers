aed0e60417e3d08eafd107eec235a7e4661540a6
Abhay V Pai <abhay@thinkpad-l470.localhost>
2020-03-28 15:46:06 +0800

    SMACSS, OOCSS, ITCSS, BEM Method

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Detect Speakers
 
 ## [Detect Speakers](https://abhaypai.github.io/app-detect-speakers/)
+
+# Rules followed in this Project.
+#   1. SCSS Rules
+## Folder architecture of scss is based on [SMACSS]() viz.
+- [Base](http://smacss.com/book/type-base).
+- [Layout](http://smacss.com/book/type-layout).
+- [Module](http://smacss.com/book/type-module).
+- [State](http://smacss.com/book/type-state).
+- Page (Root file viz. custom made ) (which may or may not contains above all scss file) (inspiration from [ITCSS](https://www.hongkiat.com/blog/inverted-triangle-css-web-development/)).
+
+## All SCSS files from above architecture directory must follow:
+- One class must do one thing properly.
+- [BEM](http://getbem.com/introduction/) seperator for class defining. eg(.navbar--logo__link)
+- Object oriented CSS selector. [OOCSS](https://www.keycdn.com/blog/oocss)
+- Avoid styling using id.
+- Avoid using element selector directly.
+- Avoid deep selector (not greater than 3).
--- src/assets/js/Components/Navbar.js
+++ src/assets/js/Components/Navbar.js
@@ -1,63 +1,73 @@
 /*
  *  All Usable Libraries in this File
  */
-import React from 'react';
+import React, { useState } from 'react';
 
 const Navbar = () => {
-    return (
-        <React.Fragment>
-            <nav className='navbar navbar-expand-md bg-primary navbar-dark'>
-                <a className='nav-link' title='Abhay Pai' href='https://abhaypai.github.io/profile/web/#/about' aria-current='page'>
-                    <img src='./images/ap-logo.jpg' alt='Abhay Pai' className='img-fluid' />
-                </a>
-                <button className='navbar-toggler border' type='button' data-toggle='collapse' data-target='#mainmenu'>
-                    <span className='navbar-toggler-icon' />
-                </button>
-                <div className='collapse navbar-collapse' id='mainmenu'>
-                    <ul className='navbar-nav ml-auto'>
-                        <li className='nav-item'>
-                            <a className='nav-link' href='https://abhaypai.github.io/profile/web/#/about' aria-current='page'>
-                                About
-                            </a>
-                        </li>
-                        <li className='nav-item'>
-                            <a className='nav-link' href='https://abhaypai.github.io/profile/web/#/skills'>
-                                Skills
-                            </a>
-                        </li>
-                        <li className='nav-item'>
-                            <a className='nav-link' href='https://abhaypai.github.io/profile/web/#/education'>
-                                Education
-                            </a>
-                        </li>
-                        <li className='nav-item'>
-                            <a className='nav-link' href='https://abhaypai.github.io/profile/web/#/work-experience'>
-                                Work Experience
-                            </a>
-                        </li>
-                        <li className='nav-item'>
-                            <a className='nav-link active' href='https://abhaypai.github.io/profile/web/#/project'>
-                                Project
-                            </a>
-                        </li>
-                        <li className='nav-item'>
-                            <a className='nav-link' href='https://abhaypai.github.io/profile/web/#/basic-frameworks'>
-                                Basic Frameworks
-                            </a>
-                        </li>
-                        <li className='nav-item'>
-                            <a className='nav-link' href='https://abhaypai.github.io/profile/web/#/concepts'>
-                                Concepts
-                            </a>
-                        </li>
-                        <li className='nav-item'>
-                            <a className='nav-link' href='https://abhaypai.github.io/profile/web/#/' />
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </React.Fragment>
-    );
+  const [mobileMenu, setMobileMenu] = useState(true);
+
+  return (
+    <React.Fragment>
+      <nav className='navbar'>
+        <div className='navbar--logo'>
+          <a className='navbar--logo__link' title='Abhay Pai' href='https://abhaypai.github.io/profile/web/#/about'>
+            <img src='./images/ap-logo.jpg' alt='Abhay Pai' className='navbar--logo__img' />
+          </a>
+        </div>
+        <button className={mobileMenu ? 'navbar--hamburger show' : 'navbar--hamburger hide'} onClick={() => setMobileMenu(!mobileMenu)}>
+          <svg viewBox='0 0 100 80' width='40' height='40'>
+            <rect width='100' height='20' />
+            <rect y='30' width='100' height='20' />
+            <rect y='60' width='100' height='20' />
+          </svg>
+        </button>
+        <button className={mobileMenu ? 'navbar--x hide' : 'navbar--x show'} onClick={() => setMobileMenu(!mobileMenu)}>
+          <svg viewBox='0 0 40 40' width='40' height='40'>
+            <path d='M 10,10 L 30,30 M 30,10 L 10,30' />
+          </svg>
+        </button>
+        <div className='navbar--menu'>
+          <ul className={mobileMenu ? 'navbar--menu__header hide' : 'navbar--menu__header show'}>
+            <li className='navbar--menu__list'>
+              <a className='navbar--menu__link' href='https://abhaypai.github.io/profile/web/#/about'>
+                About
+              </a>
+            </li>
+            <li className='navbar--menu__list'>
+              <a className='navbar--menu__link' href='https://abhaypai.github.io/profile/web/#/skills'>
+                Skills
+              </a>
+            </li>
+            <li className='navbar--menu__list'>
+              <a className='navbar--menu__link' href='https://abhaypai.github.io/profile/web/#/education'>
+                Education
+              </a>
+            </li>
+            <li className='navbar--menu__list'>
+              <a className='navbar--menu__link' href='https://abhaypai.github.io/profile/web/#/work-experience'>
+                Work Experience
+              </a>
+            </li>
+            <li className='navbar--menu__list'>
+              <a className='navbar--menu__link active' href='https://abhaypai.github.io/profile/web/#/project'>
+                Project App Detector
+              </a>
+            </li>
+            <li className='navbar--menu__list'>
+              <a className='navbar--menu__link' href='https://abhaypai.github.io/profile/web/#/basic-frameworks'>
+                Basic Frameworks
+              </a>
+            </li>
+            <li className='navbar--menu__list'>
+              <a className='navbar--menu__link' href='https://abhaypai.github.io/profile/web/#/concepts'>
+                Concepts
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
 };
 
 export default Navbar;
